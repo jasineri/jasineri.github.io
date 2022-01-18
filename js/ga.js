8f78c5336bff5d3ea402b588ae6fa270d64b5443
jasineri <jasineri@gmx.de>
2022-01-18 17:37:47 +0100

    add js

---
+++ js/ga.js
@@ -0,0 +1,1 @@
+(function(i,r){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();Common.getScript('http://www.google-analytics.com/analytics.js')})(window,'ga');ga('create','UA-48680933-1',{'storage':'none'});ga('send','pageview');
