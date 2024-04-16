7bd20c870dc470c2affb931374f23d0a0529a6d4
jasineri <jasineri@gmx.de>
2024-04-16 17:26:13 +0200

    use ga4

--- test/js/ga.js
+++ test/js/ga.js
@@ -1,1 +1,4 @@
-Common.getScript('https://www.googletagmanager.com/gtag/js?id=G-8MSTJDEDZ3',function(){window.dataLayer=window.dataLayer||[];dataLayer.push({'js':new Date,'config':'G-8MSTJDEDZ3'})})
+Common.getScript('https://www.googletagmanager.com/gtag/js?id=G-8MSTJDEDZ3', function () {
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({'js': new Date(), 'config': 'G-8MSTJDEDZ3'});
+});
