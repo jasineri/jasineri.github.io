b0381ecb37bf682953a039f2fceab99f52dc0d8d
jasineri <jasineri@gmx.de>
2024-05-24 11:34:22 +0200

    update ga4

--- test/js/ga.js
+++ test/js/ga.js
@@ -1,6 +1,6 @@
 Common.getScript('https://www.googletagmanager.com/gtag/js?id=G-KEEEWBM2FB',function(){
     window.dataLayer = window.dataLayer || [];
-    dataLayer.push('js', new Date());
-    dataLayer.push('config', 'G-KEEEWBM2FB');
+    dataLayer.push({js: new Date()});
+    dataLayer.push({config: 'G-KEEEWBM2FB'});
 }
 )
