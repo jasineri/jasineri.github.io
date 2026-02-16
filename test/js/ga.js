b17ca92a25e496a089affba534d80c752ef0b556
jasineri <jasineri@gmx.de>
2026-02-16 14:53:45 +0100

    Revert "test new vfx"
    This reverts commit be0ff1f14756caaefda1e8e05c3e54e25446e654.

---
+++ test/js/ga.js
@@ -0,0 +1,1 @@
+Common.getScript("https://www.googletagmanager.com/gtag/js?id=G-7980FYF6VM",function(){function a(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[];a("js",new Date);a("config","G-7980FYF6VM")});
